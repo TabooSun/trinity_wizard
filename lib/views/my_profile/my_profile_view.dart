c7b73949c87cca8630901831025bfc75b1a822b9
TabooSun <taboosun1996@gmail.com>
2024-06-26 20:35:09 +0800

    Complete profile

--- lib/views/my_profile/my_profile_view.dart
+++ lib/views/my_profile/my_profile_view.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:trinity_wizard/services/storage/dto/user_dto.dart';
+import 'package:trinity_wizard/theme/app_colors.dart';
 import 'package:trinity_wizard/views/my_profile/my_profile_view_model.dart';
 
 class MyProfileView extends StatefulWidget {
@@ -16,6 +18,109 @@ class _MyProfileViewState extends State<MyProfileView> {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'My Profile',
+        ),
+        actions: <Widget>[
+          TextButton(
+            onPressed: vm.logout,
+            child: const Text(
+              'Logout',
+            ),
+          ),
+        ],
+      ),
+      body: Obx(() {
+        final UserDto? contact = vm.contact.value;
+        if (contact == null) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+        return Column(
+          children: <Widget>[
+            SizedBox.square(
+              dimension: 100,
+              child: DecoratedBox(
+                decoration: const ShapeDecoration(
+                  shape: CircleBorder(),
+                  color: Color(0xff0077b6),
+                ),
+                child: Center(
+                  child: Text(
+                    (contact.firstName[0] + contact.lastName[0]).toUpperCase(),
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 30,
+                      fontWeight: FontWeight.w200,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Text(
+              '${contact.firstName} ${contact.lastName}',
+              style: const TextStyle(
+                fontSize: 15,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+            if (contact.email != null) ...<Widget>[
+              const SizedBox(
+                height: 10,
+              ),
+              Text(
+                '${contact.email}',
+                style: const TextStyle(
+                  fontSize: 15,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+            ],
+            if (contact.dob != null) ...<Widget>[
+              const SizedBox(
+                height: 10,
+              ),
+              Text(
+                '${contact.dob}',
+                style: const TextStyle(
+                  fontSize: 15,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+            ],
+            const SizedBox(
+              height: 25,
+            ),
+            SizedBox(
+              width: double.infinity,
+              height: 53,
+              child: TextButton(
+                onPressed: vm.editProfile,
+                style: TextButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  backgroundColor: const Color(0x1A96D3F2),
+                  foregroundColor: AppColors.primary,
+                ),
+                child: const Text(
+                  'Update my detail',
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w900,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        );
+      }),
+    );
   }
 }
--- lib/views/my_profile/my_profile_view_model.dart
+++ lib/views/my_profile/my_profile_view_model.dart
@@ -1,4 +1,46 @@
 import 'package:get/get.dart';
+import 'package:trinity_wizard/routes/route_collections.dart';
+import 'package:trinity_wizard/services/storage/dto/user_dto.dart';
+import 'package:trinity_wizard/services/storage/storage_key_name.dart';
+import 'package:trinity_wizard/services/storage/storage_service.dart';
+import 'package:trinity_wizard/services/user/user_service.dart';
+import 'package:trinity_wizard/views/contact_details/contact_details_view_model_data.dart';
 
 class MyProfileViewModel extends GetxController {
+  final StorageService _storageService = Get.find<StorageService>();
+  final UserService _userService = Get.find<UserService>();
+
+  final Rxn<UserDto> contact = Rxn<UserDto>();
+
+  @override
+  void onInit() {
+    fetchContactDetail();
+    super.onInit();
+  }
+
+  Future<void> logout() async {
+    await _storageService.prefs.remove(StorageKeyName.loggedInUserId);
+    await Get.offAllNamed(RouteName.login);
+  }
+
+  void fetchContactDetail() {
+    String? loggedInUserId =
+        _storageService.prefs.getString(StorageKeyName.loggedInUserId);
+    if (loggedInUserId == null) {
+      return;
+    }
+    contact.value = _userService.getUserByIdOrEmail(
+      credential: loggedInUserId,
+    );
+  }
+
+  Future<void> editProfile() async {
+    await Get.toNamed(
+      RouteName.contactDetails,
+      arguments: ContactDetailsViewModelData(
+        userId: contact.value!.id,
+      ),
+    );
+    fetchContactDetail();
+  }
 }
